2249b7b8a74ee39bc9fd34865fac642865044794
kaori takahashi <anohitohaima@ymobile.ne.jp>
2026-06-21 07:38:16 +0900

    変更

--- .history/src/main_20260621073210.ts
+++ .history/src/main_20260621073210.ts
@@ -1,6 +1,6 @@
 import { Mage } from "./mage";
 import { Warrior } from "./warrior";
-...
+
 // const character = new Character("太郎", 200); // abstract class はインスタンス化出来ないのでエラーが出る
 
 const warrior = new Warrior("アーサー", 100, "エクスカリバー");
