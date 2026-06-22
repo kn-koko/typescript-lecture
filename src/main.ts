609f287b6ad682bf564335545bb58358ffccf5db
meteorpoiplayer-ops <meteorpoiplayer@gmail.com>
2026-06-22 22:35:18 +0900

    enemyクラス導入に伴う変更

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { Mage } from "./mage";
 import { Warrior } from "./warrior";
-import { Character } from "./character.ts"
+import { Enemy } from "./enemy";
 
 // const character = new Character("太郎", 200); // abstract class はインスタンス化出来ないのでエラーが出る
 
@@ -11,3 +11,10 @@ warrior.attack();
 const mage = new Mage("メディア", 80);
 mage.showStatus();
 mage.attack();
+
+const slime = new Enemy("スライム", 50);
+slime.showStatus();
+slime.attack();
+slime.takeDamage(40);
+slime.showStatus();
+slime.attack();
