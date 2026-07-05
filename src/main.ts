49eb0568c46ee91f164a61a9d06fe43ae3c4dfe4
Ikuma Nishi <roman2419@outlook.jp>
2026-07-05 23:28:54 +0900

    warriorとenemyの対戦が成立するか検証

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
-import { Mage } from "./mage";
+//import { Mage } from "./mage";
 import { Warrior } from "./warrior";
 import { Enemy } from "./enemy";
-import { Archer } from "./archer";
+//import { Archer } from "./archer";
+import { battle } from "./battle";
 
 // const character = new Character("太郎", 200); // abstract class はインスタンス化出来ないのでエラーが出る
 
@@ -11,14 +12,16 @@ slime.takeDamage(40);
 slime.showStatus();
 
 const warrior = new Warrior("アーサー", 100, "エクスカリバー", 30);
-const mage = new Mage("メディア", 80, 40);
-const archer = new Archer("エミヤ", 60, 10, 20);
+//const mage = new Mage("メディア", 80, 40);
+//const archer = new Archer("エミヤ", 60, 10, 20);
 
-const party = [warrior, mage, archer];
+//const party = [warrior, mage, archer];
 
-for (const member of party) {
-  member.showStatus();
-  member.attack(slime);
-}
+battle(warrior, slime);
 
-slime.attack(party[0]);
+//for (const member of party) {
+//member.showStatus();
+//member.attack(slime);
+//}
+
+//slime.attack(party[0]);
