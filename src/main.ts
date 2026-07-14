39be14b50cad200961cda5608bfed3903a6a18bf
kn-koko <r.k.v.1225kaito@gmail.com>
2026-07-15 01:05:14 +0900

    lecture8-c カスタムエラーとそれによる例外処理の分岐

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,26 @@
+class CalculationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "CalculationError";
+  }
+}
+
 function divide(a: number, b: number): number {
   if (b === 0) {
-    throw new Error("0で割ることは出来ません");
+    throw new CalculationError("0で割ることは出来ません");
   }
   return a / b;
 }
 
 try {
-  const result = divide(10, 2);
+  const result = divide(10, 0);
   console.log(result);
 } catch (error) {
-  console.error((error as Error).message);
+  if (error instanceof CalculationError) {
+    console.log("計算のやり直し");
+  } else {
+    console.log("予期せぬエラー: ", error);
+  }
 } finally {
   console.log("この処理は絶対に実行される");
 }
