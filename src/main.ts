78b0baf3a5e056d5db090a7201613cf41b2e78be
kn-koko <r.k.v.1225kaito@gmail.com>
2026-07-15 01:04:35 +0900

    lecture8-b 例外の基礎2 (finally, 自分で例外を発生させる)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-console.log("test1");
-
-try {
-  const obj = JSON.parse("{ invalid json }");
-  console.log("この行は実行されない");
-} catch (error) {
-  console.log("エラーが発生しました: ", error);
+function divide(a: number, b: number): number {
+  if (b === 0) {
+    throw new Error("0で割ることは出来ません");
+  }
+  return a / b;
 }
 
-console.log("この行は実行される");
+try {
+  const result = divide(10, 2);
+  console.log(result);
+} catch (error) {
+  console.error((error as Error).message);
+} finally {
+  console.log("この処理は絶対に実行される");
+}
