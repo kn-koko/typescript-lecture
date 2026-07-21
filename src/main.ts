76009fabb28b03440d087a10def2bacad0f8daea
kn-koko <r.k.v.1225kaito@gmail.com>
2026-07-21 20:37:16 +0900

    lecture8 カスタムエラー ValidationError の追加

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
+class ValidationError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "ValidationError";
+  }
+}
+
 function divide(a: number, b: number): number {
   if (b === 0) {
-    throw new Error("0で割ることは出来ません");
+    throw new ValidationError("0で割ることは出来ません");
   }
   return a / b;
 }
@@ -9,7 +16,9 @@ try {
   console.log(divide(10, 2))
   console.log(divide(10, 0))
 } catch (error) {
-  console.error((error as Error).message);
+  if (error instanceof ValidationError) {
+    console.error(error);
+  }
 } finally {
   console.log("計算終了")
 }
